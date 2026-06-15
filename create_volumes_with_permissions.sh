31bddb339e81c55e2d44bdb7766c91ecf49f8fcb
SylvainGuilbaud <sylvain.guilbaud@intersystems.com>
2026-06-15 13:32:35 +0200

    init

---
+++ create_volumes_with_permissions.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# This script is used to set the correct permissions on the persistent volume 
+# in order to allow the IRIS container to read and write to it without issues.
+
+# Get the last directory of $PWD to replace docker_ in volume_name
+source .env
+
+# Get the instance name from the first argument 
+# If no argument is provided, use the default IRIS instance name from .env
+instance_name=${1:-$IRIS_INSTANCE_NAME}
+
+set_permissions() {
+    local volume_name="${IRIS_INSTANCE_NAME}_$1"
+    local mount_point="/$1"
+    docker run --rm -v "${volume_name}:${mount_point}" alpine sh -c \
+        "chown -R 51773:51773 ${mount_point} && chmod -R u+rwX,g+rwX ${mount_point}"
+}
+
+# Set permissions for the persistent volumes
+set_permissions "databases_"$instance_name
+set_permissions "journal_"$instance_name
+set_permissions "journal2_"$instance_name
+set_permissions "WIJ_"$instance_name    
---
+++ start.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Start script for iris-training
+# This script is used to start the IRIS container and ensure that the correct permissions are set on the persistent volumes.
+source .env
+# Set permissions on the persistent volumes
+echo "Setting permissions on persistent volumes..."
+create_volumes_with_permissions.sh $IRIS_INSTANCE_NAME
+
+# Copy the .env.test file to .env to ensure that the correct environment variables are used for the training instance
+cp .env.test .env
+
+# Start the containers
+echo "Starting the containers..."
+docker-compose -p "${IRIS_INSTANCE_NAME}" up -d
+if [ $? -ne 0 ]; then
+    echo "ERROR: Failed to start the containers."
+    exit 1
+else
+    echo "Containers started successfully."    
+fi
+
+# Copy the .env.public file to .env to ensure that the sensitive environment variables are kept private and not exposed in the public .env file
+cp .env.public .env
