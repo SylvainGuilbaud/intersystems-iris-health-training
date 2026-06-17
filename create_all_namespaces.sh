6a37e98e0325fa2114d125fb6b0644898fd7a815
SylvainGuilbaud <sylvain.guilbaud@intersystems.com>
2026-06-17 17:09:56 +0200

    init

--- create_all_namespaces.sh
+++ create_all_namespaces.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for name in Delphine Danmark Marck-Augustus Carl-Jamie Francois Rochelle Neil Adrian Philippe Jean-Michel Olivier Michael Sophie
+for name in Delphine Danmark Marck-Augustus Carl-Jamie Francois Rochelle Neil Adrian Philippe Jean-Michel Olivier Michael Sophie Frederic
 do
     ./delete_namespace.sh $name dev-aws -y
     ./create_namespace.sh $name dev-aws -y
