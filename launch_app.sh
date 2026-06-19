3acd8e7e2e49d2137b1380abba0b03d95943c0dc
SylvainGuilbaud <sylvain.guilbaud@intersystems.com>
2026-06-19 18:03:38 +0200

    init

--- launch_app.sh
+++ launch_app.sh
@@ -1,1 +1,12 @@
-cd iris/angular && npx ng serve
+#!/usr/bin/env bash
+set -e
+
+cd "$(dirname "$0")/iris/angular"
+
+# Install dependencies on first run (node_modules is git-ignored)
+if [ ! -d node_modules ]; then
+  echo "Installing Angular dependencies (first run)..."
+  npm install
+fi
+
+npx ng serve
