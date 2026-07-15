40c0c9aef381befe2bd5bfd598e29902d021985d
SylvainGuilbaud <sylvain.guilbaud@intersystems.com>
2026-07-15 22:51:58 +0200

    init

---
+++ scp-from-aws.sh
@@ -0,0 +1,8 @@
+# if only one argument, copy to home directory
+# if two arguments, copy to second argument directory
+source cloudenv
+if [ "$#" -eq 1 ]; then
+    set -- "$1" "$1"
+fi
+
+scp -r -i $ACCESS_KEY_FILENAME $CLOUD_USERNAME@$PUBLIC_DNS:$1 $2
